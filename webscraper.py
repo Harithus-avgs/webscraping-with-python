46f8912a53b2c8feb7d4c7ceef47e66c0b360b4a
Harithus Avgs <hariavgs@gmail.com>
2021-03-06 15:02:03 +0530

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-driver.get('https://www.youtube.com/c/DisguisedToast/videos')
+driver.get('paste the link of videos section of the channel')
 
-driver.find_element_by_xpath('//*[@id="thumbnail"]').click()
+driver.find_element_by_xpath('paste the xpath of latest video of the channel by inspecting').click()
